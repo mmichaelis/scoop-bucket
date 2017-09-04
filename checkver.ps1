1e91367e9b6335c05dea0bd6b23aa431f53a1aa3
Mark Michaelis <michaelis.mark@gmail.com>
2017-09-04 14:54:47 +0200

    Add checkver-script

---
+++ checkver.ps1
@@ -0,0 +1,9 @@
+# See https://github.com/lukesampson/scoop/issues/97
+
+param($app) #optionally pass an app name to just check one app
+
+$bin = split-path (scoop which scoop)
+$checkver = "$bin\checkver.ps1"
+$dir = "$psscriptroot" # checks the current dir; modify if you need something else
+
+& $checkver -app $app -dir $dir
