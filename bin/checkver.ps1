3924a48e13eb1f3023aa6a9715585731cadd78d1
Mark Michaelis <michaelis.mark@gmail.com>
2017-09-12 20:56:10 +0200

    Adjust checkver to example from scoop-extras bucket.

--- bin/checkver.ps1
+++ bin/checkver.ps1
@@ -1,9 +1,11 @@
-# See https://github.com/lukesampson/scoop/issues/97
-
-param($app) #optionally pass an app name to just check one app
-
-$bin = split-path (scoop which scoop)
-$checkver = "$bin\checkver.ps1"
-$dir = "$psscriptroot" # checks the current dir; modify if you need something else
-
-& $checkver -app $app -dir $dir
+###
+### See https://github.com/lukesampson/scoop/issues/97
+### See https://github.com/lukesampson/scoop-extras/blob/master/bin/checkver.ps1
+###
+### If you are asked to change the execution policy when trying to
+### execute this script, use: Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+###
+if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
+$checkver = "$env:SCOOP_HOME/bin/checkver.ps1"
+$dir = "$psscriptroot/.." # checks the parent dir
+iex -command "$checkver -dir $dir $($args |% { "$_ " })"
